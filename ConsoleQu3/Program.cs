3c383bc706bca448fd72ca7dc6a58d8d54c52822
Noite Cetafaju <Noite.Cetafaju@labfies.org.br>
2022-03-28 20:37:13 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleQu3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleQu3
+{
+    class Program
+    {
+        static void Main()
+        {
+            double nm1, nm2;
+
+            Console.Write("Digite um número: ");
+            nm1 = double.Parse(Console.ReadLine());
+            Console.Write("Digite outro número: ");
+            nm2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Adição: " + (nm1 + nm2));
+            Console.WriteLine("Subtração: " + (nm1 - nm2));
+            Console.WriteLine("Multiplicação " + (nm1 * nm2));
+            Console.WriteLine("Divisão: " + (nm1 / nm2));
+            Console.ReadKey();
+            
+
+
+        }
+    }
+}
